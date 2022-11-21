74a8048c226d37678f92251ccbe49dd771fe06e6
unknown <amir@gmail.com>
2022-11-21 19:17:57 +0330

    fuck this vim bullshit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function time_cal(typeofTIME,opi,value){
-    //hello there
+UWEIFH OEWIUF[HJ O9[UIFWH N[FR09WH R0[WRJE3W R0=DNVUDPJDWQ	=ERF-I VKWEQIKHDFB EFLVJENWLFJBFLWKFJNERIOPQWMNCWQEKJHRFIQWURFERTIF123H40[3429230-93-904I Czxa	qw
+function time_cal(typeofTIME,opi,value){function time_cal(typeofTIME,opi,value){ //hello there
     var d = new Date();
     var tempT = d.getTime()
     switch(typeofTIME){
@@ -20,20 +20,34 @@ function time_cal(typeofTIME,opi,value){
             value *= 60000;
             switch(opi){
                 case('+'):
-                    tempT+=value;
+                    tempT+=valuef
                     var date = new Date(tempT);
                     return date;   
                 case('-'):
+                    var date = new Date(tempT);
                     tempT-=value;
                     var date = new Date(tempT);
                     return date;
             }
-        case('hours'):
+		    sssssak;lsdhsakudaddsgerhgtrhyl'ighro';hjdaassss
+
+
+
+
+
+
+
+dfsdffff
+		    ;
+
+		    221
             value *= 3600000;
             switch(opi){
                 case('+'):
                     tempT+=value;
                     var date = new Date(tempT);
+			    perjoijiouy8pi879-;'lf,p;'gmbkn's\   
+			    F
                     return date;
                 case('-'):
                     tempT-=value;
@@ -54,6 +68,54 @@ function time_cal(typeofTIME,opi,value){
             }
     }
 }
-console.log(time_cal('days','+',1)
+FDFDFDFDFR3ER2311231231312321313
+
+\\
+\SAD
+AS\D
+SD\ASDSAWSWSWSWSWSDWSWSDWSD--QW
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
